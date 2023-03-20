fa923954bd38d8cf02a95469c77d3d3cdb3dba29
nrougier <rougiern@gmail.com>
2023-03-20 19:55:47 -0300

    fetch data & validations

--- src/components/PokeApi.jsx
+++ src/components/PokeApi.jsx
@@ -6,49 +6,57 @@ export default function PokeApi() {
   const [search, setSearch] = useState(1);
   const [image, setImage] = useState("");
   const [namePokemon, setNamePokemon] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  const url = `https://pokeapi.co/api/v2/pokemon/${search}`;
 
   const handleSearchName = (e) => {
+    setError(false);
+    item === "" ? setSearch("mr-mime") : setSearch(item);
     e.preventDefault();
-    setSearch(item);
   };
 
   const handleSearchRandom = (e) => {
+    setError(false);
     e.preventDefault();
     let random = Math.floor(Math.random() * 248);
     setSearch(random);
   };
 
   useEffect(() => {
-    const url = `https://pokeapi.co/api/v2/pokemon/${search}`;
-    const getPokemon = async (url) => {
-      try {
-        let res = await fetch(url);
-        let json = await res.json();
-        const imgPokemon =
-          json.sprites.other["official-artwork"]["front_default"];
-        const namePokemon = json.name;
-        setImage(imgPokemon);
-        setNamePokemon(namePokemon);
-      } catch (e) {
-        console.info(e);
-      }
-    };
-
-    getPokemon(url);
+    setLoading(true);
+    try {
+      fetch(url).then((res) =>
+        !res.ok
+          ? setError(true)
+          : res.json().then((data) => {
+              setImage(data.sprites.other["official-artwork"]["front_default"]);
+              setNamePokemon(data.name);
+              setLoading(false);
+            })
+      );
+    } catch (e) {
+      console.log(e);
+    }
   }, [search]);
 
   return (
     <>
       <h1>/ / PokeAPI</h1>
-      <form action="">
+      <form action="submit">
         <input onChange={(e) => setItem(e.target.value)} type="text" />
         <button onClick={handleSearchName}>Buscar</button>
         <button onClick={handleSearchRandom}>Aleatorio</button>
       </form>
-      <div>
-        <h2>{namePokemon.toUpperCase()}</h2>
-        <img src={image} alt="" />
-      </div>
+      {error ? (
+        <h2>Error en la busqueda o Pokemon inexistente</h2>
+      ) : (
+        <div>
+          <h2>{loading ? "CARGANDO..." : namePokemon.toUpperCase()}</h2>
+          <img src={image} alt="" />
+        </div>
+      )}
     </>
   );
 }
